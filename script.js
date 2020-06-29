00070cfb7a5d5a23eba3ed40beab24365693689c
gostevVitalii <62056973+gostevVitalii@users.noreply.github.com>
2020-06-30 00:30:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+$('#element').typeIt({
+    speed: 100,
+    autoStart: false
+})
+.tiType('Hello! My name is Tom Riddle')
+.tiPause(2000)
+.tiDelete()
+.tiType('Шучу, конечно) Это ж я, Лис')
+.tiPause(1000)
+.tiBreak() .tiPause(500)
+.tiType('Вот, новую штуку пробую...')
+.tiBreak() .tiPause(750)
+.tiType('Прикольно так)))')
